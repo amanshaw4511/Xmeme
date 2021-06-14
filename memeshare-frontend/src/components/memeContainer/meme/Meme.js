530744414f6b6506ab4d5e237b6555cfcc93ca1b
Aman Shaw <amanshaw4511@gmail.com>
2021-06-14 11:06:56 +0530

    added lazy image loading

--- memeshare-frontend/src/components/memeContainer/meme/Meme.js
+++ memeshare-frontend/src/components/memeContainer/meme/Meme.js
@@ -1,11 +1,36 @@
-import { Card, Col, Row } from "react-bootstrap";
+import {useState, useEffect} from "react";
+import {Card, Col, Row} from "react-bootstrap";
 import MemeOptions from "./MemeOptions";
 import "./Meme.css";
 
-const Meme = ({ meme, getMemes }) => {
-  const onEditMeme = (e) => {
-    console.log(e);
-  };
+const AsyncImage = ({src}) => {
+  const [loadedSrc, setLoadedSrc] = useState(null);
+
+  useEffect(() => {
+    setLoadedSrc(null);
+    if (src) {
+      const handleLoad = () => {
+        setLoadedSrc(src);
+      };
+      const image = new Image();
+      image.addEventListener('load', handleLoad);
+      image.src = src;
+      return () => {
+        image.removeEventListener('load', handleLoad);
+      };
+    };
+  }, [src]);
+
+  if (loadedSrc === src) {
+    return (
+      <img src={src} />
+    );
+  }
+  return null;
+}
+
+const Meme = ({meme, getMemes}) => {
+
   return (
     <div className="Meme">
       <Card>
@@ -22,7 +47,8 @@ const Meme = ({ meme, getMemes }) => {
         <Card.Body>
           <Card.Text>{meme.caption}</Card.Text>
         </Card.Body>
-        <Card.Img src={meme.url} />
+        {/* <Card.Img src={meme.url} />*/}
+        <AsyncImage src={meme.url} />
       </Card>
     </div>
   );
